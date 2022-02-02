51b7f7bf7cf22ec8b5cdda19ba0a8b5483a4bc25
Ricardo Rodriguez <rrrodriguez@uc.cl>
2022-02-02 00:23:28 -0300

    4A

---
+++ A/4A.cpp
@@ -0,0 +1,16 @@
+//
+// Created by ricardo on 2/2/22.
+//
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    cin  >> num;
+    if ((num > 3) and (num % 2 == 0)){
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+    return 0;
+}
+
